426a181c18b73952863a079419a277856399e30f
MaxKlaxx <max@tetrisk.de>
2018-01-17 01:31:00 +0100

    read exif meta data (ISO-Rating and Exposure time)

---
+++ LightScanner/ImageExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightScanner
+{
+  public static class ImageExtensions
+  {
+    public static int GetIsoRating(this Image image)
+    {
+      var p = image.GetPropertyItem(0x8827);
+
+      return BitConverter.ToInt16(p.Value, 0);
+    }
+
+    public static Tuple<int, int> GetExposureTime(this Image image)
+    {
+      var p = image.GetPropertyItem(0x829a);
+
+      return new Tuple<int, int>(BitConverter.ToInt32(p.Value, 0), BitConverter.ToInt32(p.Value, 4));
+    }
+  }
+}
--- LightScanner/Program.cs
+++ LightScanner/Program.cs
@@ -1,15 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LightScanner
 {
-  class Program
+  static class Program
   {
-    static void Main(string[] args)
+    const string TestPhotos = @"..\..\..\testphotos\";
+
+    static void Main()
     {
+      foreach (var file in new DirectoryInfo(TestPhotos).GetFiles("*.jpg"))
+      {
+        Console.Write("{0} ({1:N1} kByte) ", file.Name, file.Length / 1024.0);
+        var pic = Image.FromFile(file.FullName);
+
+        int iso = pic.GetIsoRating();
+        var exp = pic.GetExposureTime();
+        double expTime = exp.Item1 / (double)exp.Item2;
+        double lightMulti = iso * expTime;
+
+
+        Console.WriteLine(" - mul: {0:N2}", lightMulti);
+      }
     }
   }
 }
