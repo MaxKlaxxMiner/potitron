02b1db120a4affb9e21f8436c06d60d9afd5f9cd
MaxKlaxxMiner <max@tetrisk.de>
2020-04-06 22:21:26 +0200

    neustart: MNA

--- Network/Program.cs
+++ Network/Program.cs
@@ -20,6 +20,16 @@ using System.Threading.Tasks;
 // ReSharper disable CollectionNeverQueried.Local
 // ReSharper disable UnusedParameter.Global
 // ReSharper disable VirtualMemberNeverOverriden.Global
+// ReSharper disable FieldCanBeMadeReadOnly.Global
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable NotAccessedField.Global
+// ReSharper disable ClassCanBeSealed.Global
+// ReSharper disable ForCanBeConvertedToForeach
+// ReSharper disable UnusedVariable
+// ReSharper disable CollectionNeverQueried.Global
+// ReSharper disable RedundantIfElseBlock
+#pragma warning disable 414
+#pragma warning disable 219
 #pragma warning disable 649
 #pragma warning disable 169
 #endregion
@@ -28,139 +38,76 @@ namespace Network
 {
   class Program
   {
-    class Node
-    {
-      public double volt;
-      public List<Element> connected = new List<Element>();
-      public Node(double volt)
-      {
-        this.volt = volt;
-      }
-      public override string ToString()
-      {
-        return volt.ToString("N1") + " V";
-      }
-    }
-
-    class FixNode : Node
-    {
-      public FixNode(double volt) : base(volt) { }
-      public override string ToString()
-      {
-        return volt == 0 ? "Ground" : "Source " + base.ToString();
-      }
-    }
-
-    class WireNode : Node
-    {
-      public WireNode() : base(0.0) { }
-    }
-
-    abstract class Element
+    public abstract class Component
     {
       public Node node1;
       public Node node2;
-      public double ampere;
-      public FixNode FixNode { get { return node1 as FixNode ?? node2 as FixNode; } }
-    }
-
-    class Resistor : Element
-    {
-      public double ohm;
-      public Resistor(double ohm, Node node1, Node node2)
+      protected Component(Node node1, Node node2)
       {
-        this.ohm = 1 / ohm;
         this.node1 = node1;
         this.node2 = node2;
-        node1.connected.Add(this);
-        node2.connected.Add(this);
-      }
-      public override string ToString()
-      {
-        return (1 / ohm).ToString("N0") + " ohm, " + (ampere * 1000.0).ToString("N1") + " mA, " + Math.Abs(node1.volt - node2.volt).ToString("N1") + " V";
+        node1.components.Add(this);
+        node2.components.Add(this);
       }
     }
 
-    static void UpdateNetwork(Node anyNode)
+    public class VoltageSource : Component
     {
-      // --- dazugehäörige Netzwerk-Elemente sammeln ---
+      public double volt;
+      public VoltageSource(double volt, Node node1, Node node2) : base(node1, node2) { this.volt = volt; }
+      public override string ToString() { return "S: " + volt.ToString("N1"); }
+    }
+
+    public class Resistor : Component
+    {
+      public double ohm;
+      public Resistor(double ohm, Node node1, Node node2) : base(node1, node2) { this.ohm = ohm; }
+      public override string ToString() { return "R: " + ohm.ToString("N0"); }
+    }
+
+    public class Node
+    {
+      public List<Component> components = new List<Component>();
+    }
+
+    public static void Calc(Node ground)
+    {
+      // --- Netzwerk scannen ---
       var nodes = new HashSet<Node>();
-      var elements = new HashSet<Element>();
+      var components = new HashSet<Component>();
       Action<Node> addNode = null;
       addNode = n =>
       {
         if (n == null || nodes.Contains(n)) return;
         nodes.Add(n);
-        foreach (var el in n.connected)
+        foreach (var el in n.components)
         {
-          elements.Add(el);
+          components.Add(el);
           addNode(el.node1);
           addNode(el.node2);
         }
       };
-      addNode(anyNode);
-
-      // --- alle möglichen Maschen suchen ---
-      var allMeshes = new List<Element[]>();
-      foreach (var n in nodes.OrderBy(x => -x.volt))
-      {
-        var fix = n as FixNode;
-        if (fix == null) continue;
-        allMeshes.AddRange(Gen(fix, fix, new List<Element>()).Select(list => list.ToArray()));
-      }
-
-      // --- linear unabhängig Maschen ermitteln ---
-      var usedElements = new HashSet<Element>();
-      var meshes = new List<Element[]>();
-      foreach (var newMesh in allMeshes.OrderBy(x => x.Last().FixNode.volt).ThenBy(x => x.First().FixNode.volt))
-      {
-        if (newMesh.All(el => usedElements.Contains(el))) continue;
-        foreach (var el in newMesh) usedElements.Add(el);
-        meshes.Add(newMesh);
-      }
+      addNode(ground);
 
 
-    }
-
-    static IEnumerable<List<Element>> Gen(FixNode first, Node node, List<Element> list)
-    {
-      foreach (var el in node.connected)
-      {
-        if (list.Contains(el)) continue;
-
-        var nextNode = el.node1 != node ? el.node1 : el.node2;
-
-        list.Add(el);
-
-        if (nextNode is FixNode)
-        {
-          if (nextNode.volt < first.volt) yield return list;
-        }
-        else
-        {
-          foreach (var masche in Gen(first, nextNode, list))
-          {
-            yield return masche;
-          }
-        }
-
-        list.RemoveAt(list.Count - 1);
-      }
     }
 
     static void Main(string[] args)
     {
       Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
 
-      var ground = new FixNode(0);
-      var u1 = new FixNode(10);
-      var u2 = new FixNode(20);
-      var w = new WireNode();
-      var r1 = new Resistor(10, u1, w);
-      var r2 = new Resistor(20, w, ground);
-      var r3 = new Resistor(30, w, u2);
+      var ground = new Node();
+      var wire10V = new Node();
+      var wire20V = new Node();
+      var wireMiddle = new Node();
 
-      UpdateNetwork(ground);
+      var source10V = new VoltageSource(10, ground, wire10V);
+      var source20V = new VoltageSource(20, ground, wire20V);
+      var r10 = new Resistor(10, wire10V, wireMiddle);
+      var r20 = new Resistor(20, wireMiddle, ground);
+      var r30 = new Resistor(30, wireMiddle, wire20V);
+
+      Calc(ground);
     }
   }
 }
