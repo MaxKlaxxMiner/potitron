faf9cf62cc3332dd47fe5c08caa970062dda2d3a
MaxKlaxxMiner <max@tetrisk.de>
2020-04-06 23:08:25 +0200

    matrixG fertig berechnet

--- Network/Program.cs
+++ Network/Program.cs
@@ -55,14 +55,14 @@ namespace Network
     {
       public double volt;
       public VoltageSource(double volt, Node node1, Node node2) : base(node1, node2) { this.volt = volt; }
-      public override string ToString() { return "S: " + volt.ToString("N1"); }
+      public override string ToString() { return "S: " + volt.ToString("N1") + " V"; }
     }
 
     public class Resistor : Component
     {
       public double ohm;
       public Resistor(double ohm, Node node1, Node node2) : base(node1, node2) { this.ohm = ohm; }
-      public override string ToString() { return "R: " + ohm.ToString("N0"); }
+      public override string ToString() { return "R: " + ohm.ToString("N0") + " ohm"; }
     }
 
     public class Node
@@ -73,21 +73,56 @@ namespace Network
     public static void Calc(Node ground)
     {
       // --- Netzwerk scannen ---
-      var nodes = new HashSet<Node>();
-      var components = new HashSet<Component>();
+      var nodesHash = new HashSet<Node>();
+      var componentsHash = new HashSet<Component>();
       Action<Node> addNode = null;
-      addNode = n =>
+      addNode = x =>
       {
-        if (n == null || nodes.Contains(n)) return;
-        nodes.Add(n);
-        foreach (var el in n.components)
+        if (x == null || nodesHash.Contains(x)) return;
+        nodesHash.Add(x);
+        foreach (var el in x.components)
         {
-          components.Add(el);
+          componentsHash.Add(el);
           addNode(el.node1);
           addNode(el.node2);
         }
       };
       addNode(ground);
+
+      var nodes = nodesHash.Where(x => x != ground).ToArray();
+      var voltageSources = componentsHash.Where(x => x is VoltageSource).ToArray();
+
+      int n = nodes.Length;
+      int m = voltageSources.Length;
+
+      //  matrixA: (m + n) * (m + n) - main matrix
+      //    matrixG: n * n - interconnections between the passive circuit elements
+      //    matrixB: n * m - connection of the voltage sources
+      //    matrixC: m * n - connection of the voltage sources
+      //    matrixD: m * m - zero if only independent sources are considered
+
+      // --- Matrix mit den Gleitwerten berechnen ---
+      var matrixG = new double[n, n];
+      for (int y = 0; y < n; y++)
+      {
+        for (int x = 0; x < n; x++)
+        {
+          if (x == y) // Hauptdiagonale = Summe der Leitwerte aller Zweige, die mit Knoten x verbunden sind
+          {
+            foreach (var c in nodes[x].components)
+            {
+              var r = c as Resistor;
+              if (r == null) continue;
+              matrixG[x, y] += 1 / r.ohm;
+            }
+          }
+          else // negative Summe der Leitwerte zwischen den benachbarten Knoten i und j (Koppelleitwerte). Besteht keine direkte Verbindung zwischen zwei Knoten, wird an dieser Stelle eine Null eingetragen.
+          {
+            var rs = nodes[x].components.Where(c => c is Resistor && nodes[y].components.Contains(c)).Cast<Resistor>().ToArray();
+            matrixG[x, y] = -rs.Sum(r => 1 / r.ohm);
+          }
+        }
+      }
 
 
     }
@@ -96,16 +131,36 @@ namespace Network
     {
       Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
 
-      var ground = new Node();
-      var wire10V = new Node();
-      var wire20V = new Node();
-      var wireMiddle = new Node();
+      //var ground = new Node();
+      //var wire10V = new Node();
+      //var wire20V = new Node();
+      //var wireMiddle = new Node();
 
-      var source10V = new VoltageSource(10, ground, wire10V);
-      var source20V = new VoltageSource(20, ground, wire20V);
-      var r10 = new Resistor(10, wire10V, wireMiddle);
-      var r20 = new Resistor(20, wireMiddle, ground);
-      var r30 = new Resistor(30, wireMiddle, wire20V);
+      //var source10V = new VoltageSource(10, ground, wire10V);
+      //var source20V = new VoltageSource(20, ground, wire20V);
+      //var r10 = new Resistor(10, wire10V, wireMiddle);
+      //var r20 = new Resistor(20, wireMiddle, ground);
+      //var r30 = new Resistor(30, wireMiddle, wire20V);
+
+      // --- case 1 ---
+      //var ground = new Node();
+      //var node1 = new Node();
+      //var node2 = new Node();
+      //var node3 = new Node();
+      //var r1 = new Resistor(2, node1, ground);
+      //var r2 = new Resistor(4, node2, node3);
+      //var r3 = new Resistor(8, node2, ground);
+      //var source32V = new VoltageSource(32, node2, node1);
+      //var source20V = new VoltageSource(20, node3, ground);
+
+      // --- case 2 ---
+      var ground = new Node();
+      var node1 = new Node();
+      var node2 = new Node();
+      var r1 = new Resistor(2, node1, ground);
+      var r2 = new Resistor(4, node1, node2);
+      var r3 = new Resistor(8, node2, ground);
+      var source32V = new VoltageSource(32, node1, node2);
 
       Calc(ground);
     }
